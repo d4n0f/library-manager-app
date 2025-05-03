b2b9aadd79ab72a490eb04b5c793c503932896b0
Bálint Fónad <fonadbalint04@gmail.com>
2025-05-03 18:31:57 +0200

    feat(api): add Book model
    Added Book.cs to the models with the given validations including EF generated key, required and non whitespace only attributes, non negative PublicationYear.

---
+++ LibraryManager.API/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.API.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int InventoryNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?!\s*$)")]
+        public string Title { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?!\s*$)")]
+        public string Author { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?!\s*$)")]
+        public string Publisher { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int PublicationYear { get; set; }
+    }
+}
