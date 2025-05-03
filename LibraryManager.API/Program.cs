266da7316be81ea57279c95d5f907c90b663f916
Bálint Fónad <fonadbalint04@gmail.com>
2025-05-04 00:32:00 +0200

    feat(api): configure Swagger, integrate SQLite, and update Program.cs for API setup
    Updated Program.cs for swagger use, added launchUrl to launchSettings.json for swagger and modified applicationUrl, also added ConnectionStrings to appsettings.json.

--- LibraryManager.API/Program.cs
+++ LibraryManager.API/Program.cs
@@ -1,17 +1,33 @@
+using LibraryManager.API;
+using LibraryManager.API.Services;
+using LibraryManager.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DemoDataContext>(
+    options =>
+    {
+        options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
+        options.UseLazyLoadingProxies();
+    });
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IBookService, BookService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
