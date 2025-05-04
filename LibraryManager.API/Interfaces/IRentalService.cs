c914b7ac0c21d0be756de1ab53290d72b8e17967
Bálint Fónad <fonadbalint04@gmail.com>
2025-05-04 23:29:59 +0200

    feat(api): implement Rental service logic
    Implemented RentalService and IRentalService for service operations.

---
+++ LibraryManager.API/Interfaces/IRentalService.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManager.API.Models;
+
+namespace LibraryManager.API.Interfaces
+{
+    public interface IRentalService
+    {
+        Rental GetRental(int readerNumber, int inventoryNumber);
+
+        List<Rental> GetAllRentals();
+
+        bool RentBook(Rental rental);
+
+        bool ReturnBook(int readerNumber, int inventoryNumber);
+    }
+}
---
+++ LibraryManager.API/Services/RentalService.cs
@@ -0,0 +1,48 @@
+﻿using LibraryManager.API.Interfaces;
+using LibraryManager.API.Models;
+
+namespace LibraryManager.API.Services
+{
+    public class RentalService : IRentalService
+    {
+        private readonly List<Rental> _rentals;
+
+        public RentalService()
+        {
+            _rentals = [];
+        }
+
+        public List<Rental> GetAllRentals()
+        {
+            return _rentals;
+        }
+
+        public Rental GetRental(int readerNumber, int inventoryNumber)
+        {
+            return _rentals.Find(x => x.InventoryNumber == inventoryNumber && x.ReaderNumber == readerNumber);
+        }
+
+        public bool RentBook(Rental rental)
+        {
+            if (_rentals.Any(x => x.InventoryNumber == rental.InventoryNumber && x.ReaderNumber == rental.ReaderNumber))
+            {
+                return false;
+            }
+            _rentals.Add(rental);
+            return true;
+        }
+        
+        public bool ReturnBook(int readerNumber, int inventoryNumber)
+        {
+            var rental = _rentals.Find(x => x.InventoryNumber == inventoryNumber && x.ReaderNumber == readerNumber);
+
+            if (rental == null)
+            {
+                return false;
+            }
+
+            _rentals.Remove(rental);
+            return true;
+        }
+    }
+}
