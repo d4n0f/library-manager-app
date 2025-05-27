03222d121d57ded16a22f71989de8b9d27df41d0
Bálint Fónad <fonadbalint04@gmail.com>
2025-05-27 03:09:28 +0200

    feat(database): add new migration, update old migration

--- LibraryManager.API/Migrations/20250525215936_AddRentalsTable.Designer.cs
+++ LibraryManager.API/Migrations/20250525215936_AddRentalsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LibraryManager.API;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryManager.API.Migrations
 {
     [DbContext(typeof(LibraryDataContext))]
-    partial class LibraryDataContextModelSnapshot : ModelSnapshot
+    [Migration("20250525215936_AddRentalsTable")]
+    partial class AddRentalsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.36");
---
+++ LibraryManager.API/Migrations/20250525215936_AddRentalsTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManager.API.Migrations
+{
+    public partial class AddRentalsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rentals",
+                columns: table => new
+                {
+                    RentalID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ReaderNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    InventoryNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    RentalDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    DueDate = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rentals", x => x.RentalID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rentals");
+        }
+    }
+}
