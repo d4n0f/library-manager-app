c33ae21116f2518ffaa710776867100071f52455
Bálint Fónad <fonadbalint04@gmail.com>
2025-11-05 00:42:38 +0100

    feat(client): hide restricted sections for non-admin users

---
+++ LibraryManager.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("role")]
+        [Authorize]
+        public async Task<IActionResult> GetUserRole()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return Unauthorized();
+
+            var roles = await _userManager.GetRolesAsync(user);
+            return Ok(roles.FirstOrDefault() ?? "User");
+        }
+    }
+}
